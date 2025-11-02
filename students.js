415a16218518171745b9323d17423f6e3132dfda
chingizpro <chingizprog@gmail.com>
2025-11-02 14:35:39 +0500

    Update files and contents

---
+++ students.js
@@ -0,0 +1,8 @@
+const students = [
+  { name: "Frank", age: 30, address: "London" },
+  { name: "Yunus", age: 26, address: "Tashkent" },
+  { name: "Tom", age: 20, address: "Sydney" },
+  { name: "Sveta", age: 27, address: "Moscow" },
+  { name: "Jane", age: 24, address: "London" },
+  { name: "Jack", age: 25, address: "Sydney" },
+];
